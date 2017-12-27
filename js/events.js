5129091d81dc0394f93b0b5330f8d26c1b242e87
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-27 05:33:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function pressIt() {
 
 function submitIt() {
   $("form").on("submit", function() {
-    alert('your form is going to be submitted now');
+    alert("Your form is going to be submitted now.");
   });
 }
 
