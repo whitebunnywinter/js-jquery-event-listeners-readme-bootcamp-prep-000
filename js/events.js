bca759fc2eb46fae36dcbf1a2a8e8c7d9c481eb1
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-27 05:32:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,11 +19,17 @@ function pressIt() {
   });
 }
 
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('your form is going to be submitted now');
+  });
+}
+
 $(document).ready(function(){
 
   // call functions here
   getIt();
   frameIt();
-  pressIt()
+  pressIt();
 
 });
